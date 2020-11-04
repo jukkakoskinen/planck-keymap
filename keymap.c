bf2c91b8c987b977300e2d37d7aa50a92a81042e
Jukka Koskinen <jukka@koskinen.io>
2020-11-04 22:56:18 +0200

    Swap esc and tab, add hash

--- keymap.c
+++ keymap.c
@@ -20,14 +20,14 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DEFAULT] = LAYOUT_planck_grid(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
         _______, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
     [_LOWER] = LAYOUT_planck_grid(
         _______, FI_EXLM, FI_DQUO, XY_EURO, FI_SLSH, FI_LABK, FI_LPRN, FI_LBRC, XY_LCBR, FI_PLUS, FI_ASTR, KC_DEL,
-        _______, FI_QUES, FI_QUOT, FI_DLR,  XY_BSLS, FI_RABK, FI_RPRN, FI_RBRC, XY_RCBR, FI_MINS, FI_PERC, _______,
+        _______, FI_QUES, FI_QUOT, FI_DLR,  XY_BSLS, FI_RABK, FI_RPRN, FI_RBRC, XY_RCBR, FI_MINS, FI_PERC, FI_HASH,
         _______, FI_CIRC, _______, _______, _______, _______, _______, _______, _______, FI_EQL,  FI_TILD, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
